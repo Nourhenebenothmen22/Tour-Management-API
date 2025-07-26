8ccac10cb90ce4d269dafeeb728b7a74bbd97331
nourhene <benothmennourhen8@gmail.com>
2025-07-26 20:52:25 +0100

    📝 Ajout du contrôleur createReview pour associer un avis à un tour

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,1 +1,22 @@
-const Review=require("../models/Review");
+const Review = require("../models/Review");
+const Tour = require("../models/Tour");
+
+exports.createReview = async (req, res) => {
+  const tourId = req.params.tourId;
+
+  try {
+    // Créer et sauvegarder l'avis
+    const newReview = new Review({ ...req.body});
+    const savedReview = await newReview.save();
+
+    // Ajouter la référence de l'avis dans le modèle Tour (relation)
+    await Tour.findByIdAndUpdate(tourId, {
+      $push: { reviews: savedReview._id },
+    });
+
+    res.status(201).json(savedReview);
+  } catch (error) {
+    console.error("Erreur lors de la création de l'avis :", error);
+    res.status(500).json({ message: error.message });
+  }
+};

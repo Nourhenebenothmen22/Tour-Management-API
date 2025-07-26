66cb1d8757470723f6e4b4e684abd5cac3fabbbe
nourhene <benothmennourhen8@gmail.com>
2025-07-26 21:30:15 +0100

    🚏 Ajout des routes GET : getAllReviews, getReviewById et getReviewsByTourId

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {
-  createReview,
+  createReview,getReviews,getReviewsbyId 
 } = require('../controllers/reviewController'); // Importer les contrôleurs de revue
 router.post("/:tourId", createReview); // Créer un nouvel avis
+// Obtenir tous les avis d’un tour
+router.get("/", getReviews);
+router.get('/:id', getReviewsbyId ); // Récupérer un avis par ID
+// Exporter le routeur pour l'utiliser dans l'application principale
 module.exports = router;
